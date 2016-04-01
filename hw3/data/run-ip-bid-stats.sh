e3a102d2df3f6f5e85c2eec23a7978308f60d136
dzenin <dzenin@edmunds.com>
2016-04-01 19:09:05 +0300

    Update hw3

--- hw3/data/run-ip-bid-stats.sh
+++ hw3/data/run-ip-bid-stats.sh
@@ -4,4 +4,4 @@ hdfs dfs -rm -r  /apps/bdc/hw3/output/
 
 DATASET=hdfs://sandbox.hortonworks.com:8020/apps/bdc/hw3/dataset
 OUTPUT_DIR=hdfs://sandbox.hortonworks.com:8020/apps/bdc/hw3/output/
-hadoop jar target/hw3-1.0.jar com.epam.bdc.tag.IpBidStatsApp ./target/lib/UserAgentUtils-1.19.jar $DATASET $OUTPUT_DIR
+hadoop jar target/hw3-1.0.jar com.epam.bdc.bid.IpBidStatsApp ./target/lib/UserAgentUtils-1.19.jar $DATASET $OUTPUT_DIR
