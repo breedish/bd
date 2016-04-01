9c2e1e51d9ef8d7ff6697465558c5e63cb29e20b
dzenin <dzenin@edmunds.com>
2016-04-01 18:53:23 +0300

    Update

--- hw3/data/run-count-tag.sh
+++ hw3/data/run-count-tag.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-INPUT_DIR=/opt/tmp/hw3/tag/
-OUTPUT_DIR=/tmp/output/tags
-hadoop jar target/hw3-1.0.jar com.epam.bdc.tag.CountTagApp -files ./target/user.tags.txt -libjars ./target/lib/UserAgentUtils-1.19.jar $INPUT_DIR $OUTPUT_DIR
+hdfs dfs -rm -r  /apps/bdc/hw3/output/
+hdfs dfs -mkdir -p /apps/bdc/hw3/output/
+
+DATASET=hdfs://sandbox.hortonwroks.com:8020/apps/bdc/hw3/dataset
+OUTPUT_DIR=hdfs://sandbox.hortonwroks.com:8020/apps/bdc/hw3/output/
+hadoop jar target/hw3-1.0.jar com.epam.bdc.tag.CountTagApp -files ./target/user.tags.txt -libjars ./target/lib/UserAgentUtils-1.19.jar $DATASET $OUTPUT_DIR
