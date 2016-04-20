f8917ca2f47b664ff8e1dca6545bb0019416f587
dzenin <dzenin@edmunds.com>
2016-04-20 15:23:31 +0300

    add hw5-udf

--- hw4/src/main/java/com/epam/bdc/IPinYouJob.java
+++ hw4/src/main/java/com/epam/bdc/IPinYouJob.java
@@ -11,6 +11,13 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
+import java.util.IntSummaryStatistics;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.reducing;
+
 /**
  * @author zenind
  */
@@ -56,7 +63,68 @@ public class IPinYouJob extends Configured implements Tool {
         return result;
     }
 
+    public static int doSeq(int n) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += new Integer(i);
+        }
+        return sum;
+    }
+
+    public static int doSeq2(int n) {
+        return Stream.iterate(0, (Integer i) -> i + 1)
+            .limit(n)
+            .reduce((i, j) -> i + j).get();
+    }
+
+    public static int doPar(int n) {
+        return IntStream.rangeClosed(1, n)
+            .boxed()
+            .reduce(0, (i, j) -> i + j);
+    }
+
+    public static int doPar2(int n) {
+        return IntStream.rangeClosed(1, n)
+            .parallel()
+            .reduce(0, Integer::sum);
+    }
+
     public static void main(String[] args) throws Exception {
-        System.exit(ToolRunner.run(new IPinYouJob(), args));
+//        System.exit(ToolRunner.run(new IPinYouJob(), args));
+
+        int result = IntStream.rangeClosed(1, 3)
+            .boxed()
+            .collect(reducing(0, (i) -> i, (i, j) -> i + j / 2));
+        System.out.println(result);
+
+        IntSummaryStatistics stats = IntStream.rangeClosed(1, 100)
+            .boxed()
+            .collect(Collectors.summarizingInt(i -> i));
+
+        System.out.println(stats);
+
+        int n = 10000000;
+
+        long start = System.currentTimeMillis();
+        doSeq(n);
+        System.out.println("Seq = " + (System.currentTimeMillis() - start)/1000.0);
+
+        start = System.currentTimeMillis();
+        doSeq2(n);
+        System.out.println("Seq2 = " + (System.currentTimeMillis() - start)/1000.0);
+
+        start = System.currentTimeMillis();
+        doPar(n);
+        System.out.println("Par = " + (System.currentTimeMillis() - start)/1000.0);
+
+        start = System.currentTimeMillis();
+        doPar2(n);
+        System.out.println("Par2 = " + (System.currentTimeMillis() - start)/1000.0);
+
+
+
+//        Stream.iterate(new int[]{0, 1}, (int[] p) -> new int[]{p[1], p[0] + p[1]})
+//            .limit(10)
+//            .forEach((int[] p) -> System.out.print("(" + p[0] + "," + p[1] + ")"));
     }
 }
