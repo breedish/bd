b0be40b89c7ac34d165067ac1e7d065d97fc8eb4
dzenin <dzenin@edmunds.com>
2016-04-26 20:57:39 +0300

    hw5

--- hw5/src/main/java/com/epam/bd/UserAgentFunction.java
+++ hw5/src/main/java/com/epam/bd/UserAgentFunction.java
@@ -9,13 +9,36 @@ import org.apache.hadoop.io.Text;
  */
 public final class UserAgentFunction extends UDF {
 
+    private enum InfoType {
+        UA_TYPE,
+        UA_FAMILY,
+        OS,
+        DEVICE;
+    }
+
     private Text result = new Text();
 
-    public Text evaluate(Text ua) {
-        if (ua == null) {
+    public Text evaluate(Text ua, Text infoType) {
+        if (ua == null || infoType == null) {
             return null;
         }
-        result.set(UserAgent.parseUserAgentString(ua.toString()).getOperatingSystem().getDeviceType().name());
+
+        InfoType type = InfoType.valueOf(infoType.toString().toUpperCase());
+        UserAgent userAgent = UserAgent.parseUserAgentString(ua.toString());
+        switch (type) {
+            case UA_TYPE:
+                result.set(userAgent.getBrowser().getBrowserType().name());
+                break;
+            case UA_FAMILY:
+                result.set(userAgent.getBrowser().getManufacturer().name());
+                break;
+            case OS:
+                result.set(userAgent.getOperatingSystem().name());
+                break;
+            case DEVICE:
+                result.set(userAgent.getOperatingSystem().getDeviceType().name());
+                break;
+        }
         return result;
     }
 
